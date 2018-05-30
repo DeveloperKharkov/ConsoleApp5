8d8d85b18d028f507bc624ffe813aababcd43221
Alexey-Borzenkov <alexey.borzenkov@zoral.com.ua>
2018-05-30 07:30:49 +0300

    Task with circle

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -10,6 +10,30 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
+            double radius = 10;
+
+
+            for (double i = 1d; i < 2*radius; i++)
+            {
+                double y = Math.Sqrt(radius*radius-Math.Pow(i-radius, 2));
+                if (y < 0)
+                    break;
+
+                int top = 20 + (int)Math.Round(y);
+                int top2 = 20 - (int)Math.Round(y);
+
+                if (top < 0)
+                    break;
+
+                Console.SetCursorPosition(5 + (int)Math.Round(i) + 10, top);
+                Console.Write("*");
+                Console.SetCursorPosition(5 + (int)Math.Round(i) + 10, top2);
+                Console.Write("*");
+
+            }
+            Console.ReadKey();
+            Console.MoveBufferArea(1, 1, 50, 50, 5, 5);
         }
     }
 }
+
