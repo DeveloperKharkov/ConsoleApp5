fcce4c646cec92d57a7ff8e9d3040395fffd508f
B-18-02 <B-18-02@T02>
2018-06-02 12:57:47 +0300

    Module 8 workshop

---
+++ Module7ClassWork/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module7ClassWork
+{
+    struct Book
+    {
+        public string Title;
+        public string Author;
+        public int NumberOfPages;
+        public int Id;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] books = new Book[4];
+            books[0] = new Book
+            {
+                Author = "AA",
+                NumberOfPages = 12213,
+                Title = "dsd",
+                Id = 255633
+            };
+
+            books[1] = new Book
+            {
+                Author = "Ghehe",
+                NumberOfPages = 12,
+                Title = "drrttd",
+                Id = 24533
+            };
+
+            books[2] = new Book
+            {
+                Author = "BB",
+                NumberOfPages = 13,
+                Title = "dsd",
+                Id = 2344333
+            };
+
+            books[3] = new Book
+            {
+                Author = "F",
+                NumberOfPages = 1573,
+                Title = "dsfddfd",
+                Id = 233
+            };
+
+            for (int i = books.Length - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (books[j].NumberOfPages > books[j + 1].NumberOfPages)
+                    {
+                        int tempNumberOfPages = books[j + 1].NumberOfPages;
+                        books[j + 1].NumberOfPages = books[j].NumberOfPages;
+                        books[j].NumberOfPages = tempNumberOfPages;
+                    }
+                }
+            }
+
+            foreach (Book item in books)
+            {
+                Console.WriteLine(item.NumberOfPages);
+            }
+
+            Console.WriteLine("Output for orderedBooks:");
+
+            IOrderedEnumerable<Book> orderedBooks = books.OrderBy(b => b.);
+
+            foreach (Book item in orderedBooks)
+            {
+                Console.WriteLine(item.Author);
+            }
+        }
+    }
+}
---
+++ Module8ClassWork/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module8ClassWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Задача 7.9
+            В заданном массиве найти минимальное и 
+            максимальное значение.
+            Вывести на экран массив, после чего максимальное 
+            и минимальное значение с указанием номера этих 
+            элементов в массиве. */
+            int[] array = { 1, 4, 5, 3, 7, 45, 67, 89 };
+            int min = int.MaxValue, max = int.MinValue;
+
+            int positionOfMinValue = -1;
+            int positionOfMaxValue = -1;
+
+            for (int i=0; i<array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    positionOfMaxValue = i + 1;
+                }
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    positionOfMinValue = i + 1;
+                }
+            }
+
+            Console.WriteLine("List of all elements of array:");
+
+            foreach (int item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"min={min}, position of min element={positionOfMinValue}");
+            Console.WriteLine($"max={max}, position of max element={positionOfMaxValue}");
+        }
+    }
+}
