79305f78e360f61c67fdbcd20cdbbb7ab39cffb6
B-18-02 <B-18-02@T02>
2018-06-14 20:15:11 +0300

    Task 9.3 added

--- Module10ClassWork/Program.cs
+++ Module10ClassWork/Program.cs
@@ -6,10 +6,54 @@ using System.Threading.Tasks;
 
 namespace Module10ClassWork
 {
+    public class TreeNode
+    {
+        public TreeNode ParentNode;
+        public int NodeValue;
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static public void Main(string[] args)
         {
+            var nodeList = new List<TreeNode>();
+
+            int[,] treeNodes = { {5, 2}, {1, 6}, {5, 4}, {3, 5}, {5, 7}, {3, 1} };
+
+            for (int i = 0; i < treeNodes.GetLength(0); i++)
+            {
+                TreeNode node = nodeList.Find(x => x.NodeValue == treeNodes[i, 1]);
+
+                if (node == null)
+                {
+                    node = new TreeNode { NodeValue = treeNodes[i, 1] };
+                    nodeList.Add(node);
+                }
+
+                TreeNode parentNode = nodeList.Find(x => x.NodeValue == treeNodes[i, 0]);
+
+                if (parentNode == null)
+                {
+                    parentNode = new TreeNode { NodeValue = treeNodes[i, 0] };
+                    nodeList.Add(parentNode);
+                }
+
+                node.ParentNode = parentNode;
+            }
+
+            foreach (var node in nodeList)
+            {
+                TreeNode parentNode = node;
+                int level = 0;
+                do
+                {
+                    parentNode = parentNode.ParentNode;
+                    level++;
+                }
+                while (parentNode != null);
+
+                Console.WriteLine($"Node={node.NodeValue}, level={level}");
+            }
         }
     }
 }
