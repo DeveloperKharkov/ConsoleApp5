86d27eaf8dcf91919b33a0644e1f112c7ad566a7
B-18-02 <B-18-02@T02>
2018-06-19 20:49:47 +0300

    Module 12 homework

---
+++ ItCloud.Education.MathLibs/Incrementor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItCloud.Education.MathLibs
+{
+    public static class Incrementor
+    {
+        public static int Inc(ref int val1, ref int val2)
+        {
+            ++val1;
+            ++val2;
+            return 3;
+        }
+
+        public static int Inc(ref int val1, ref int val2, ref int val3)
+        {
+            ++val1;
+            ++val2;
+            ++val3;
+            return 3;
+        }
+    }
+}
---
+++ Module12ClassWork/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ItCloud.Education.MathLibs;
+
+namespace Module12ClassWork
+{
+    public struct Product
+    {
+        public int ID;
+        public string Name;
+        public decimal Price;
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Id = {ID}, Name = {Name}, Price = {Price}");
+        }
+
+        public Product(int id, string name, decimal price)
+        {
+            ID = id;
+            Name = name;
+            Price = price;
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Product[] arrayOfProducts;
+            Console.WriteLine("Please input number of products:");
+            int numberOfProducts = int.Parse(Console.ReadLine());
+
+            arrayOfProducts = new Product[numberOfProducts];
+
+            for (int i = 0; i < numberOfProducts; i++)
+            {
+
+            }
+        }
+    }
+}
