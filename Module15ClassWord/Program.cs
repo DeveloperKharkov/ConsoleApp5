4f2c19bbaf38d07980a73d4af31126773631af65
B-18-02 <B-18-02@T02>
2018-06-30 13:07:36 +0300

    Module 15 added

---
+++ Module15ClassWord/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Module15ClassWord
+{
+    public class Book
+    {
+        private string _author;
+        private string _title;
+        private int _numberOfPages;
+
+        public Book()
+        {
+            _author = "unknown";
+            _title = "unknown";
+            _numberOfPages = 0;
+        }
+
+        public Book(string author, string title)
+        {
+            _author = author;
+            _title = title;
+            _numberOfPages = 0;
+        }
+
+        public Book(string author, string title, int numberOfPages)
+        {
+            _author = author;
+            _title = title;
+            _numberOfPages = numberOfPages;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
