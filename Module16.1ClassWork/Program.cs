90e16a5def1a44cd427f72b6fc852286471e9bca
B-18-02 <B-18-02@T02>
2018-07-10 19:51:49 +0300

    Module 16 class  work part 1

---
+++ Module16.1ClassWork/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module16._1ClassWork
+{
+    abstract class Figure
+    {
+        public abstract double Area();
+    }
+
+    class Square : Figure
+    {
+        public int Side { get; private set; }
+
+        public override double Area()
+        {
+            return Side * Side;
+        }
+
+        public Square(int side)
+        {
+            Side = side;
+        }
+    }
+
+    class Circle : Figure
+    {
+        public int Radius { get; private set; }
+
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure[] array = new Figure[2];
+            array[0] = new Square(10);
+            array[1] = new Circle(10);
+
+            foreach (Figure item in array)
+            {
+                Console.WriteLine($"Area for {item.GetType().Name} equal {item.Area()}");
+            }
+        }
+    }
+}
---
+++ Module16HomeWork/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module16HomeWork
+{
+    static class TemeratureConveror
+    {
+        public static float FromCelsiusToKelvin(float value)
+        {
+            return value + 273.15f;
+        }
+
+        public static float FromKelvinToCelsius(float value)
+        {
+            return value - 273.15f;
+        }
+
+        public static float FromCelsiusToFahrenheit(float value)
+        {
+            return value*1.8f + 32;
+        }
+
+        public static float FromFahrenheitToCelsius(float value)
+        {
+            return (32 - value) / 1.8f;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input value which you want to convert:");
+            float inputValue = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please choose the operation:");
+            Console.WriteLine("1 - FromCelsius To Kelvin");
+            Console.WriteLine("2 - FromKelvin To Celsius");
+            Console.WriteLine("3 - FromCelsius To Fahrenheit");
+            Console.WriteLine("4 - FromFahrenheit To Celsius");
+
+            byte operation = byte.Parse(Console.ReadLine());
+            float result;
+            switch(operation)
+            {
+                case 1:
+                    result = TemeratureConveror.FromCelsiusToKelvin(inputValue);
+                    break;
+                case 2:
+                    result = TemeratureConveror.FromKelvinToCelsius(inputValue);
+                    break;
+                case 3:
+                    result = TemeratureConveror.FromCelsiusToFahrenheit(inputValue);
+                    break;
+                case 4:
+                    result = TemeratureConveror.FromFahrenheitToCelsius(inputValue);
+                    break;
+                default:
+                    throw new Exception($"the operation='{operation}' is unsupported");
+            }
+
+            Console.WriteLine($"Result after conversion equal to {result}");
+        }
+    }
+}
